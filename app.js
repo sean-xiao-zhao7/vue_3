a9697cf62da1414f7ef96f4a8d38d301f8e7198d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-02-17 15:05:45 -0500

    Add goals

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
-  data() {
-    return { goals: [] };
-  },
+    data() {
+        return { goals: [], enteredGoalValue: "" };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredGoalValue);
+        },
+        deleteGoal(id) {
+            this.goals.splice(id, 1);
+        },
+    },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
